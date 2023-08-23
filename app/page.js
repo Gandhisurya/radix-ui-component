ff3f97497ab9646b28ae2d58d3bfc317f6f8f27b
Gandhisurya <gandhisurya508@gmail.com>
2023-08-23 19:37:45 +0530

    tabs add

--- app/page.js
+++ app/page.js
@@ -20,6 +20,7 @@ import {
   Inset,
   Popover,
   Grid,
+  Tabs,
 } from "@radix-ui/themes";
 
 import {
@@ -398,6 +399,33 @@ export default function Home() {
             </Grid>
           </Popover.Content>
         </Popover.Root>
+      </div>
+      <div className="my-10 mx-auto w-[90vw] sm:w-[600px]">
+        <p className="font-semibold text-xl mb-5 text-center">Tabs</p>
+
+        <Tabs.Root defaultValue="account">
+          <Tabs.List>
+            <Tabs.Trigger value="account">Account</Tabs.Trigger>
+            <Tabs.Trigger value="documents">Documents</Tabs.Trigger>
+            <Tabs.Trigger value="settings">Settings</Tabs.Trigger>
+          </Tabs.List>
+
+          <Box px="4" pt="3" pb="2">
+            <Tabs.Content value="account">
+              <Text size="2">Make changes to your account.</Text>
+            </Tabs.Content>
+
+            <Tabs.Content value="documents">
+              <Text size="2">Access and update your documents.</Text>
+            </Tabs.Content>
+
+            <Tabs.Content value="settings">
+              <Text size="2">
+                Edit your profile or update contact information.
+              </Text>
+            </Tabs.Content>
+          </Box>
+        </Tabs.Root>
       </div>
     </main>
   );
