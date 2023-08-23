13c6ffab5b060b523397b506c784865e8b9423f2
Gandhisurya <gandhisurya508@gmail.com>
2023-08-23 15:40:46 +0530

    inset add

--- app/page.js
+++ app/page.js
@@ -17,10 +17,12 @@ import {
   HoverCard,
   Heading,
   IconButton,
+  Inset,
 } from "@radix-ui/themes";
 
 import {
   ExclamationTriangleIcon,
+  GitHubLogoIcon,
   InfoCircledIcon,
   MagnifyingGlassIcon,
 } from "@radix-ui/react-icons";
@@ -305,6 +307,38 @@ export default function Home() {
         <Text className="font-semibold" ml="3">
           search
         </Text>
+      </div>
+      <div className="my-10 mx-auto">
+        <Card size="3" className="w-[90vw] sm:w-[600px]">
+          <Flex>
+            <Inset side="left" mr="5">
+              <Flex
+                align="center"
+                justify="center"
+                px="7"
+                style={{ background: "#24292F", height: "100%" }}
+              >
+                <GitHubLogoIcon color="white" height="40" width="40" />
+              </Flex>
+            </Inset>
+            <Box style={{ maxWidth: 400 }}>
+              <Text as="div" color="gray" mb="1" size="2">
+                github.com
+              </Text>
+              <Text
+                size={{
+                  initial: "2",
+                  xs: "3",
+                  sm: "4",
+                  md: "5",
+                  xl: "7",
+                }}
+              >
+                Official Node.js SDK for interacting with the AcmeCorp API.
+              </Text>
+            </Box>
+          </Flex>
+        </Card>
       </div>
     </main>
   );
