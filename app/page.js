35f2506cce4bbac592d41ba0f2eb11899a873d99
Gandhisurya <gandhisurya508@gmail.com>
2023-08-23 14:36:30 +0530

    avatar property add

--- app/page.js
+++ app/page.js
@@ -76,7 +76,11 @@ export default function Home() {
           <Avatar
             src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
             fallback="S"
+            variant="soft" // soft, solid
             size="6"
+            color="cyan"
+            highContrast={false}
+            radius="full"
             mx="3"
           />
           <Avatar size="6" mx="3" fallback="S" />
