3413484ef3bf3aaf150c640a21d47f48f32433bc
Gandhisurya <gandhisurya508@gmail.com>
2023-08-23 14:06:14 +0530

    alert boc add

--- app/page.js
+++ app/page.js
@@ -1,17 +1,49 @@
 "use client";
 
-import { Flex, Text, Button } from "@radix-ui/themes";
+import { Flex, Text, Button, AlertDialog } from "@radix-ui/themes";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full mx-auto">
-      <div className="flex flex-col justify-center items-center mt-20 mx-auto">
-        <p className="text-2xl font-semibold">Hello from Radix Themes :)</p>
-        <Button className="mt-10 w-[150px]" onClick={() => alert(`april fool`)}>
-          Let's go
-        </Button>
+    <main className="flex flex-col">
+      <div className="max-w-[500px] mx-auto">
+        <Flex mt="6" direction="column" gap="2">
+          <Text className="text-2xl font-semibold">
+            Hello from Radix Themes :)
+          </Text>
+          <div className="mx-auto hover:scale-110 transition-transform  ">
+            <Button mt="3" width="5" onClick={() => alert(`april fool`)}>
+              Let's go
+            </Button>
+          </div>
+        </Flex>
       </div>
-      <div></div>
+      <div className="mx-auto mt-12">
+        <p className="font-semibold text-xl mb-5 text-center">alert box</p>
+        <AlertDialog.Root>
+          <AlertDialog.Trigger>
+            <Button color="red">Revoke access</Button>
+          </AlertDialog.Trigger>
+          <AlertDialog.Content style={{ maxWidth: 450 }}>
+            <AlertDialog.Title>Revoke access</AlertDialog.Title>
+            <AlertDialog.Description size="2">
+              Are you sure? This application will no longer be accessible and
+              any existing sessions will be expired.
+            </AlertDialog.Description>
+            <Flex gap="3" mt="4" justify="end">
+              <AlertDialog.Cancel>
+                <Button variant="soft" color="gray">
+                  Cancel
+                </Button>
+              </AlertDialog.Cancel>
+              <AlertDialog.Action>
+                <Button variant="solid" color="red">
+                  Revoke access
+                </Button>
+              </AlertDialog.Action>
+            </Flex>
+          </AlertDialog.Content>
+        </AlertDialog.Root>
+      </div>
     </main>
   );
 }
