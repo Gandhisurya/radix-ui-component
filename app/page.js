6c301585bf9a27c227cd35387ea55258f0d983d1
Gandhisurya <gandhisurya508@gmail.com>
2023-08-23 15:06:18 +0530

    card add

--- app/page.js
+++ app/page.js
@@ -9,9 +9,14 @@ import {
   Avatar,
   Badge,
   Callout,
+  Card,
+  Box,
 } from "@radix-ui/themes";
 
-import { InfoCircledIcon } from "@radix-ui/react-icons";
+import {
+  ExclamationTriangleIcon,
+  InfoCircledIcon,
+} from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
@@ -131,15 +136,37 @@ export default function Home() {
           variant="surface" //"soft" | "surface" | "outline"
           color="red"
           // highContrast={true}
+          role="alert"
         >
           <Callout.Icon>
-            <InfoCircledIcon />
+            {/* <InfoCircledIcon /> */}
+            <ExclamationTriangleIcon />
           </Callout.Icon>
           <Callout.Text>
             You will need admin privileges to install and access this
             application.
           </Callout.Text>
         </Callout.Root>
+      </div>
+      <div className="my-10 mx-auto w-[90vw] sm:w-[600px]">
+        <Card>
+          <Flex gap="3" align="center">
+            <Avatar
+              size="3"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              radius="full"
+              fallback="T"
+            />
+            <Box>
+              <Text as="div" size="2" weight="bold">
+                Teodros Girmay
+              </Text>
+              <Text as="div" size="2" color="gray">
+                Engineering
+              </Text>
+            </Box>
+          </Flex>
+        </Card>
       </div>
     </main>
   );
