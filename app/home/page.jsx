937133fbf3450613ae3c5565956772036d8e162f
Gandhisurya <gandhisurya508@gmail.com>
2023-08-28 10:53:21 +0530

    class to classname

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -26,7 +26,7 @@ export default function Home() {
       </div>
       <div className="mx-auto mt-10 flex items-center">
         <div className="relative group hover:scale-105 transform transition-transform ease-in duration-200 mx-5">
-          <div class="absolute -inset-0.5 bg-gradient-to-r from-pink-500 to-yellow-500 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+          <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-500 to-yellow-500 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
           <button className="relative w-[250px] h-12 rounded-xl bg-yellow-50 font-semibold text-lg flex items-center justify-center">
             Get Started
             <svg
@@ -45,7 +45,7 @@ export default function Home() {
           </button>
         </div>
         <div className="relative group hover:scale-105 transform transition-transform ease-in duration-200 mx-5">
-          <div class="absolute px-5 -inset-0.5 bg-gradient-to-r from-pink-500 to-yellow-500 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+          <div className="absolute px-5 -inset-0.5 bg-gradient-to-r from-pink-500 to-yellow-500 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
           <button className="relative w-[250px] h-12 bg-yellow-50 rounded-xl font-semibold text-lg flex items-center justify-center">
             Learn More
             <svg
@@ -64,9 +64,9 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div class="absolute top-20 left-10 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob"></div>
-      <div class="absolute top-32 right-10 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-2000"></div>
-      <div class="absolute -bottom-10 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-4000"></div>
+      <div className="absolute top-20 left-10 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob"></div>
+      <div className="absolute top-32 right-10 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-2000"></div>
+      <div className="absolute -bottom-10 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-4000"></div>
     </div>
   );
 }
