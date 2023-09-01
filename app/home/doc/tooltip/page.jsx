b4351d3b98c1c9f7abb2bb274a62253e0867e73e
Gandhisurya <gandhisurya508@gmail.com>
2023-09-01 19:10:43 +0530

    tooltip complete

--- app/home/doc/tooltip/page.jsx
+++ app/home/doc/tooltip/page.jsx
@@ -1,23 +1,39 @@
 "use client";
-import {
-  Button,
-  Callout,
-  IconButton,
-  Link,
-  Table,
-  Tooltip,
-} from "@radix-ui/themes";
+import { IconButton, Table, Tooltip } from "@radix-ui/themes";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { useRouter } from "next/navigation";
-import {
-  ExclamationTriangleIcon,
-  InfoCircledIcon,
-  PlusIcon,
-} from "@radix-ui/react-icons";
+import { PlusIcon } from "@radix-ui/react-icons";
 
 function ToolTip() {
   const router = useRouter();
+
+  const normalContent = `
+  
+  \`\`\`
+   <Tooltip content="Add to library">
+     <IconButton radius="full">
+       <PlusIcon />
+     </IconButton>
+   </Tooltip>
+  \`\`\`
+  `;
+
+  const multiContent = `
+  
+  \`\`\`
+   <Tooltip
+     multiline
+     content="Add the selected items to your library for usage within your dashboard."  
+   >
+      <IconButton radius="full">
+         <PlusIcon />
+      </IconButton> 
+   </Tooltip>
+
+ \`\`\`
+
+  `;
 
   return (
     <div className="flex flex-col w-[1200px] h-[800px] overflow-y-scroll px-40 font-next">
@@ -41,19 +57,14 @@ function ToolTip() {
             </Table.Header>
             <Table.Body>
               <Table.Row>
-                <Table.RowHeaderCell>asChild</Table.RowHeaderCell>
+                <Table.RowHeaderCell>multiline</Table.RowHeaderCell>
                 <Table.Cell>boolean</Table.Cell>
                 <Table.Cell>false</Table.Cell>
               </Table.Row>
               <Table.Row>
-                <Table.RowHeaderCell>size</Table.RowHeaderCell>
-                <Table.Cell>Responsive | "1" | "2" | "3" |</Table.Cell>
-                <Table.Cell>"2"</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.RowHeaderCell>variant</Table.RowHeaderCell>
-                <Table.Cell>enum</Table.Cell>
-                <Table.Cell>"solid"</Table.Cell>
+                <Table.RowHeaderCell>content*</Table.RowHeaderCell>
+                <Table.Cell>ReactNode</Table.Cell>
+                <Table.Cell>-</Table.Cell>
               </Table.Row>
             </Table.Body>
           </Table.Root>
@@ -61,39 +72,38 @@ function ToolTip() {
       </div>
       <div className="max-w-[600px] mt-3">
         <p className="my-2 font-semibold text-lg">Examples</p>
-        <p className="font-semibold my-3 text-xl">1. Size</p>
-        <div className="flex flex-col items-start space-y-4"></div>
+        <p className="font-semibold my-3 text-xl">1. Normal</p>
+        <div className="mt-3 flex flex-col items-start space-y-4">
+          <Tooltip content="Add to library">
+            <IconButton radius="full">
+              <PlusIcon />
+            </IconButton>
+          </Tooltip>
+        </div>
         <p className="mt-3 font-semibold">Code</p>
         <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100">
-          {/* {fallBackContent} */}
+          {normalContent}
         </ReactMarkdown>
         <div className="mt-4">
-          <p className="my-3 font-semibold text-xl">2. Variant</p>
+          <p className="my-3 font-semibold text-xl">2. Multiline</p>
           <p className="my-2">
-            Use the variant properties to control the visual style.
+            Use the <b>multiline</b> prop to wrap content across multiple lines.
           </p>
           <div className="mt-3 flex flex-col items-start space-y-4">
-            <Tooltip content="Add to library">
+            <Tooltip
+              multiline
+              content="Add the selected items to your library for usage within your dashboard."
+            >
               <IconButton radius="full">
                 <PlusIcon />
               </IconButton>
             </Tooltip>
           </div>
           <p className="mt-3 font-semibold">Code</p>
-          <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100">
-            {/* {varientContent} */}
+          <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100 overflow-y-scroll">
+            {multiContent}
           </ReactMarkdown>
         </div>
-      </div>
-      <div className="max-w-[600px] mt-3"></div>
-      <div className="w-[130px] shadow-lg flex flex-col text-right mb-10 ml-auto rounded-lg p-2">
-        <p className="">Next</p>
-        <p
-          onClick={() => router?.push("/home/doc/card")}
-          className="text-orange-500 cursor-pointer hover:font-medium"
-        >
-          Card
-        </p>
       </div>
     </div>
   );
