3c000c3697508239d1e2a610e2118fe872260ac7
Gandhisurya <gandhisurya508@gmail.com>
2023-09-03 09:45:37 +0530

    seperator done

--- app/home/doc/seperator/page.jsx
+++ app/home/doc/seperator/page.jsx
@@ -1,5 +1,12 @@
 "use client";
-import { Button, Callout, Link, Table } from "@radix-ui/themes";
+import {
+  Button,
+  Callout,
+  Flex,
+  Link,
+  Separator,
+  Table,
+} from "@radix-ui/themes";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { useRouter } from "next/navigation";
@@ -11,87 +18,54 @@ import {
 function Seperator() {
   const router = useRouter();
 
-  const fallBackContent = `
+  const sizeContent = `
     
-   \`\`\`
-      <Callout.Root size="3">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need admin privileges to install and access this
-        application.
-      </Callout.Text>
-    </Callout.Root>
-  
-    <Callout.Root size="2">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need admin privileges to install and access this
-        application.
-      </Callout.Text>
-    </Callout.Root>
-  
-    <Callout.Root size="1">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need admin privileges to install and access this
-        application.
-      </Callout.Text>
-    </Callout.Root>
-   \`\`\`
+  \`\`\`
+   <Flex direction="column" gap="4">
+      <Separator orientation="horizontal" size="4" />
+      <Separator orientation="horizontal" size="3" />
+      <Separator orientation="horizontal" size="2" />
+      <Separator orientation="horizontal" size="1" />
+   </Flex>
+   <Flex align="center" gap="4" style={{ height: 96 }} className="mt-4">
+     <Separator orientation="vertical" size="4" />
+     <Separator orientation="vertical" size="3" />
+     <Separator orientation="vertical" size="2" />
+     <Separator orientation="vertical" size="1" />
+   </Flex>
+  \`\`\`
     
     `;
 
-  const varientContent = `
+  const colorContent = `
         
-    \`\`\`
-      <Callout.Root variant="soft">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need <Link href="#">admin privileges</Link> 
-        to install and access
-        this application.
-      </Callout.Text>
-    </Callout.Root>
-  
-    <Callout.Root variant="outline">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need <Link href="#">admin privileges</Link> 
-        to install and access
-        this application.
-      </Callout.Text>
-    </Callout.Root>
-  
-    <Callout.Root variant="surface">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need <Link href="#">admin privileges</Link> 
-        to install and access
-        this application.
-      </Callout.Text>
-    </Callout.Root>
-   \`\`\`
+  \`\`\`
+     <Flex direction="column" gap="3">
+       <Separator color="indigo" size="4" />
+       <Separator color="cyan" size="4" />
+       <Separator color="orange" size="4" />
+       <Separator color="crimson" size="4" />
+     </Flex>
+  \`\`\`
         
         `;
+
+  const orientContent = `
+  
+  \`\`\`
+  <Flex align="center" gap="4">
+    <Separator orientation="horizontal" />
+    <Separator orientation="vertical" />
+  </Flex>
+  \`\`\`
+  `;
 
   return (
     <div className="flex flex-col w-[1200px] h-[800px] overflow-y-scroll px-40 font-next">
       <div className="mt-20">
-        <p className="font-semibold text-3xl ">Card</p>
+        <p className="font-semibold text-3xl ">Separator</p>
         <p className="mt-2 font-medium">
-          Container that groups related content and actions.
+          Visually or semantically separates content.
         </p>
       </div>
       <div className="max-w-[600px] mt-3">
@@ -107,19 +81,14 @@ function Seperator() {
             </Table.Header>
             <Table.Body>
               <Table.Row>
-                <Table.RowHeaderCell>asChild</Table.RowHeaderCell>
-                <Table.Cell>boolean</Table.Cell>
-                <Table.Cell>false</Table.Cell>
-              </Table.Row>
-              <Table.Row>
                 <Table.RowHeaderCell>size</Table.RowHeaderCell>
                 <Table.Cell>Responsive | "1" | "2" | "3" |</Table.Cell>
                 <Table.Cell>"2"</Table.Cell>
               </Table.Row>
               <Table.Row>
-                <Table.RowHeaderCell>variant</Table.RowHeaderCell>
+                <Table.RowHeaderCell>color</Table.RowHeaderCell>
                 <Table.Cell>enum</Table.Cell>
-                <Table.Cell>"solid"</Table.Cell>
+                <Table.Cell>"gray"</Table.Cell>
               </Table.Row>
             </Table.Body>
           </Table.Root>
@@ -129,79 +98,55 @@ function Seperator() {
         <p className="my-2 font-semibold text-lg">Examples</p>
         <p className="font-semibold my-3 text-xl">1. Size</p>
         <div>
-          <div className="flex flex-col items-start space-y-4">
-            <Callout.Root size="3">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need admin privileges to install and access this
-                application.
-              </Callout.Text>
-            </Callout.Root>
-
-            <Callout.Root size="2">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need admin privileges to install and access this
-                application.
-              </Callout.Text>
-            </Callout.Root>
-
-            <Callout.Root size="1">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need admin privileges to install and access this
-                application.
-              </Callout.Text>
-            </Callout.Root>
-          </div>
+          <p className="my-2 font-medium">Horizontal</p>
+          <Flex direction="column" gap="4">
+            <Separator orientation="horizontal" size="4" />
+            <Separator orientation="horizontal" size="3" />
+            <Separator orientation="horizontal" size="2" />
+            <Separator orientation="horizontal" size="1" />
+          </Flex>
+          <p className="my-2 font-medium">vertical</p>
+          <Flex align="center" gap="4" style={{ height: 96 }} className="mt-4">
+            <Separator orientation="vertical" size="4" />
+            <Separator orientation="vertical" size="3" />
+            <Separator orientation="vertical" size="2" />
+            <Separator orientation="vertical" size="1" />
+          </Flex>
           <p className="mt-3 font-semibold">Code</p>
-          <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100">
-            {fallBackContent}
+          <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100 overflow-x-scroll">
+            {sizeContent}
           </ReactMarkdown>
         </div>
         <div className="mt-4">
-          <p className="my-3 font-semibold text-xl">2. Variant</p>
+          <p className="my-3 font-semibold text-xl">2. Color</p>
           <p className="my-2">
-            Use the variant properties to control the visual style.
+            Use the color prop to assign a specific color, ignoring the global
+            theme.
           </p>
-          <div className="mt-3 flex flex-col items-start space-y-4">
-            <Callout.Root variant="soft">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need <Link href="#">admin privileges</Link> to install
-                and access this application.
-              </Callout.Text>
-            </Callout.Root>
-            <Callout.Root variant="outline">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need <Link href="#">admin privileges</Link> to install
-                and access this application.
-              </Callout.Text>
-            </Callout.Root>
-            <Callout.Root variant="surface">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need <Link href="#">admin privileges</Link> to install
-                and access this application.
-              </Callout.Text>
-            </Callout.Root>
-          </div>
+          <Flex direction="column" gap="3">
+            <Separator color="indigo" size="4" />
+            <Separator color="cyan" size="4" />
+            <Separator color="orange" size="4" />
+            <Separator color="crimson" size="4" />
+          </Flex>
           <p className="mt-3 font-semibold">Code</p>
-          <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100">
-            {varientContent}
+          <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100 overflow-x-scroll">
+            {colorContent}
+          </ReactMarkdown>
+        </div>
+        <div className="mt-4">
+          <p className="my-3 font-semibold text-xl">3. Orientation</p>
+          <p className="my-2">
+            Use the orientation prop to control whether the separator is
+            horizontal or vertical.
+          </p>
+          <Flex align="center" gap="4">
+            <Separator orientation="horizontal" />
+            <Separator orientation="vertical" />
+          </Flex>
+          <p className="mt-3 font-semibold">Code</p>
+          <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100 overflow-x-scroll">
+            {orientContent}
           </ReactMarkdown>
         </div>
       </div>
