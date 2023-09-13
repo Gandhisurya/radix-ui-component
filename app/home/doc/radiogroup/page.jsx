5b4b36bb53a9feb7709a1843b539eb0d704fb45b
Gandhisurya <gandhisurya508@gmail.com>
2023-09-13 22:40:31 +0530

    radio group

--- app/home/doc/radiogroup/page.jsx
+++ app/home/doc/radiogroup/page.jsx
@@ -1,12 +1,8 @@
 "use client";
-import { Button, Callout, Link, Table } from "@radix-ui/themes";
+import { Flex, Grid, RadioGroup, Table } from "@radix-ui/themes";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { useRouter } from "next/navigation";
-import {
-  ExclamationTriangleIcon,
-  InfoCircledIcon,
-} from "@radix-ui/react-icons";
 
 function Radio() {
   const router = useRouter();
@@ -14,35 +10,7 @@ function Radio() {
   const fallBackContent = `
     
    \`\`\`
-      <Callout.Root size="3">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need admin privileges to install and access this
-        application.
-      </Callout.Text>
-    </Callout.Root>
-  
-    <Callout.Root size="2">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need admin privileges to install and access this
-        application.
-      </Callout.Text>
-    </Callout.Root>
-  
-    <Callout.Root size="1">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need admin privileges to install and access this
-        application.
-      </Callout.Text>
-    </Callout.Root>
+      
    \`\`\`
     
     `;
@@ -50,38 +18,7 @@ function Radio() {
   const varientContent = `
         
     \`\`\`
-      <Callout.Root variant="soft">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need <Link href="#">admin privileges</Link> 
-        to install and access
-        this application.
-      </Callout.Text>
-    </Callout.Root>
-  
-    <Callout.Root variant="outline">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need <Link href="#">admin privileges</Link> 
-        to install and access
-        this application.
-      </Callout.Text>
-    </Callout.Root>
-  
-    <Callout.Root variant="surface">
-      <Callout.Icon>
-        <InfoCircledIcon />
-      </Callout.Icon>
-      <Callout.Text>
-        You will need <Link href="#">admin privileges</Link> 
-        to install and access
-        this application.
-      </Callout.Text>
-    </Callout.Root>
+     
    \`\`\`
         
         `;
@@ -89,9 +26,10 @@ function Radio() {
   return (
     <div className="flex flex-col w-[1200px] h-[800px] overflow-y-scroll px-40 font-next">
       <div className="mt-20">
-        <p className="font-semibold text-3xl ">Card</p>
+        <p className="font-semibold text-3xl ">Radio Group</p>
         <p className="mt-2 font-medium">
-          Container that groups related content and actions.
+          A set of interactive radio buttons where only one can be selected at a
+          time.
         </p>
       </div>
       <div className="max-w-[600px] mt-3">
@@ -107,19 +45,24 @@ function Radio() {
             </Table.Header>
             <Table.Body>
               <Table.Row>
-                <Table.RowHeaderCell>asChild</Table.RowHeaderCell>
-                <Table.Cell>boolean</Table.Cell>
-                <Table.Cell>false</Table.Cell>
-              </Table.Row>
-              <Table.Row>
                 <Table.RowHeaderCell>size</Table.RowHeaderCell>
-                <Table.Cell>Responsive | "1" | "2" | "3" |</Table.Cell>
-                <Table.Cell>"2"</Table.Cell>
+                <Table.Cell>Responsive "1" | "2" </Table.Cell>
+                <Table.Cell>"1"</Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.RowHeaderCell>variant</Table.RowHeaderCell>
+                <Table.Cell>"classic" | "surface" | "soft"</Table.Cell>
+                <Table.Cell>"surface"</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.RowHeaderCell>color</Table.RowHeaderCell>
                 <Table.Cell>enum</Table.Cell>
-                <Table.Cell>"solid"</Table.Cell>
+                <Table.Cell>-</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.RowHeaderCell>highContrast</Table.RowHeaderCell>
+                <Table.Cell>boolean</Table.Cell>
+                <Table.Cell>-</Table.Cell>
               </Table.Row>
             </Table.Body>
           </Table.Root>
@@ -128,38 +71,17 @@ function Radio() {
       <div className="max-w-[600px] mt-3">
         <p className="my-2 font-semibold text-lg">Examples</p>
         <p className="font-semibold my-3 text-xl">1. Size</p>
+        <p className="my-2">Use the size prop to control the size.</p>
         <div>
-          <div className="flex flex-col items-start space-y-4">
-            <Callout.Root size="3">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need admin privileges to install and access this
-                application.
-              </Callout.Text>
-            </Callout.Root>
+          <Flex align="center" gap="2">
+            <RadioGroup.Root size="2" defaultValue="1">
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
 
-            <Callout.Root size="2">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need admin privileges to install and access this
-                application.
-              </Callout.Text>
-            </Callout.Root>
-
-            <Callout.Root size="1">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need admin privileges to install and access this
-                application.
-              </Callout.Text>
-            </Callout.Root>
-          </div>
+            <RadioGroup.Root size="1" defaultValue="1">
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+          </Flex>
           <p className="mt-3 font-semibold">Code</p>
           <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100">
             {fallBackContent}
@@ -168,37 +90,94 @@ function Radio() {
         <div className="mt-4">
           <p className="my-3 font-semibold text-xl">2. Variant</p>
           <p className="my-2">
-            Use the variant properties to control the visual style.
+            Use the variant prop to control the visual style.
           </p>
-          <div className="mt-3 flex flex-col items-start space-y-4">
-            <Callout.Root variant="soft">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need <Link href="#">admin privileges</Link> to install
-                and access this application.
-              </Callout.Text>
-            </Callout.Root>
-            <Callout.Root variant="outline">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need <Link href="#">admin privileges</Link> to install
-                and access this application.
-              </Callout.Text>
-            </Callout.Root>
-            <Callout.Root variant="surface">
-              <Callout.Icon>
-                <InfoCircledIcon />
-              </Callout.Icon>
-              <Callout.Text>
-                You will need <Link href="#">admin privileges</Link> to install
-                and access this application.
-              </Callout.Text>
-            </Callout.Root>
-          </div>
+          <Flex gap="2">
+            <Flex direction="column" asChild gap="2">
+              <RadioGroup.Root variant="surface" defaultValue="1">
+                <RadioGroup.Item value="1" />
+                <RadioGroup.Item value="2" />
+              </RadioGroup.Root>
+            </Flex>
+
+            <Flex direction="column" asChild gap="2">
+              <RadioGroup.Root variant="classic" defaultValue="1">
+                <RadioGroup.Item value="1" />
+                <RadioGroup.Item value="2" />
+              </RadioGroup.Root>
+            </Flex>
+
+            <Flex direction="column" asChild gap="2">
+              <RadioGroup.Root variant="soft" defaultValue="1">
+                <RadioGroup.Item value="1" />
+                <RadioGroup.Item value="2" />
+              </RadioGroup.Root>
+            </Flex>
+          </Flex>
+          <p className="mt-3 font-semibold">Code</p>
+          <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100">
+            {varientContent}
+          </ReactMarkdown>
+        </div>
+        <div className="mt-4">
+          <p className="my-3 font-semibold text-xl">3. Color</p>
+          <p className="my-2">
+            Use the color prop to assign a specific color, ignoring the global
+            theme.
+          </p>
+          <Flex gap="2">
+            <RadioGroup.Root color="indigo" defaultValue="1">
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+
+            <RadioGroup.Root color="cyan" defaultValue="1">
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+
+            <RadioGroup.Root color="orange" defaultValue="1">
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+
+            <RadioGroup.Root color="crimson" defaultValue="1">
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+          </Flex>
+
+          <p className="mt-3 font-semibold">Code</p>
+          <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100">
+            {varientContent}
+          </ReactMarkdown>
+        </div>
+        <div className="mt-4">
+          <p className="my-3 font-semibold text-xl">4. High-contrast</p>
+          <p className="my-2">
+            Use the highContrast prop to add additional contrast.
+          </p>
+          <Grid columns="3" gap="2" display="inline-grid">
+            <RadioGroup.Root variant="classic" defaultValue="1">
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+
+            <RadioGroup.Root variant="surface" defaultValue="1">
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+
+            <RadioGroup.Root variant="soft" defaultValue="1">
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+
+            <RadioGroup.Root variant="classic" defaultValue="1" highContrast>
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+
+            <RadioGroup.Root variant="surface" defaultValue="1" highContrast>
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+
+            <RadioGroup.Root variant="soft" defaultValue="1" highContrast>
+              <RadioGroup.Item value="1" />
+            </RadioGroup.Root>
+          </Grid>
           <p className="mt-3 font-semibold">Code</p>
           <ReactMarkdown className="text-sm mt-4 border p-3 max-w-[600px] rounded-lg bg-blue-100">
             {varientContent}
